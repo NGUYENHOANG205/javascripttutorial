c4e3b4efd8912686c7411d489328a418dfbb3746
thongds <thongds@gmail.com>
2020-05-31 12:07:03 +0700

    fix

--- App.js
+++ App.js
@@ -56,22 +56,27 @@ function deleteCheck(event){
 //Filter
 function filterTodo(e){
     const todos = todoList.childNodes
-    todos.forEach(function(){
-        switch(e.target.value){
+    todos.forEach(function(todo){
+        switch(e.value){
             case "all":
                 todo.style.display = 'flex';
+                break;
             case "completed":
                 if(todo.classList.contains('completed')){
                     todo.style.display = 'flex';
                 } else{
                     todo.style.display = 'none';
                 };
+                break;
             case "uncompleted":
                 if(!todo.classList.contains('completed')){
                     todo.style.display = 'flex';
                 } else{
                     todo.style.display = 'none';
                 };
-                }
+                break;
+            default :
+                break
+            }
         });
 }
